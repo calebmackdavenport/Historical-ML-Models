f146ed5381633f90a5d0b84256640e6b95aedd13
Caleb Davenport <xcmdprompt@gmail.com>
2018-06-04 09:36:34 -0500

    Update Perceptrons.py

--- Perceptrons.py
+++ Perceptrons.py
@@ -236,12 +236,16 @@ class AdalineGD(object):
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 
+# This strucutre shows what happens when we choose a learning rate that is too high
+# The errors actually increase because of overfitting
 ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
 ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
 ax[0].set_xlabel('Epochs')
 ax[0].set_ylabel('log(Sum-squared-error)')
 ax[0].set_title('Adaline - Learning rate 0.01')
 
+# This structure shows what happens when we choose a learning rate that is too low
+# The errors will continuously go down, but this would take forever to run on a huge dataset
 ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
 ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
 ax[1].set_xlabel('Epochs')
@@ -254,18 +258,12 @@ plt.show()
 
 
 
-
-
-
-
 # ## Improving gradient descent through feature scaling
 
 # standardize features
 X_std = np.copy(X)
 X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
 X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
-
-
 
 
 ada = AdalineGD(n_iter=15, eta=0.01)
